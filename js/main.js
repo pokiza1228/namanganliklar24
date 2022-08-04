916878b25a59315af23d0e77b50fb31534583c51
pokiza1228 <baxromovapokiza1@gmail.com>
2022-08-05 00:31:01 +0500

    worldwide

--- js/main.js
+++ js/main.js
@@ -1,12 +1,1 @@
-const burger = document.querySelector(".burger");
-const nav =document.querySelector(".header__nav");
-burger.addEventListener("click", function() {
-    burger.classList.toggle("burger--opened");
-    nav.classList.toggle("header__nav--opened");
-});
-
-const faqs = document.querySelector(".faqs__item-btn");
-faqs.addEventListener("click", function() {
-    faqs.classList.toggle("faqs__item-btn--opened");
-});
 
